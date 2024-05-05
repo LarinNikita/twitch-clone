7c4f9f972e1e19bce7e2d706fa78d71d7e19552f
LarinNikita <larin_nikita_m@mail.ru>
2024-05-05 12:27:48 +0700

    collapsed sidebar and hint component

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -6,7 +6,7 @@ import { Actions } from './Actions';
 
 export const Navbar = () => {
     return (
-        <nav className="fixed top-0 z-[49] flex h-20 w-full items-center justify-between bg-[#252731] px-2 shadow-sm lg:px-4">
+        <nav className="fixed top-0 z-[49] flex h-20 w-full items-center justify-between bg-[#252731] px-3.5 shadow-sm lg:px-4">
             <Logo />
             <Search />
             <Actions />
---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Wrapper } from './Wrapper';
+import { Toggle } from './Toggle';
+
+export const Sidebar = () => {
+    return (
+        <Wrapper>
+            <Toggle />
+        </Wrapper>
+    );
+};
