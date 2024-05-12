d38a28a5867804a555c6396671a178218f23d2d8
LarinNikita <larin_nikita_m@mail.ru>
2024-05-12 13:23:12 +0700

    fix secure data

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { Stream, User } from '@prisma/client';
-
 import { useViewerToken } from '@/hooks/use-viewer-token';
 import { useChatSidebar } from '@/store/use-chat-sidebar';
 import { cn } from '@/lib/utils';
@@ -14,12 +12,28 @@ import { Header, HeaderSkeleton } from './header';
 import { InfoCard } from './info-card';
 import { AboutCard } from './about-card';
 
+type CustomStream = {
+    id: string;
+    isLive: boolean;
+    isChatDelayed: boolean;
+    isChatEnabled: boolean;
+    isChatFollowersOnly: boolean;
+    thumbnailUrl: string | null;
+    name: string;
+};
+
+type CustomUser = {
+    id: string;
+    username: string;
+    bio: string | null;
+    imageUrl: string;
+    stream: CustomStream | null;
+    _count: { followedBy: number };
+};
+
 interface StreamPlayerProps {
-    user: User & {
-        stream: Stream | null;
-        _count: { followedBy: number };
-    };
-    stream: Stream;
+    user: CustomUser;
+    stream: CustomStream;
     isFollowing: boolean;
 }
 
